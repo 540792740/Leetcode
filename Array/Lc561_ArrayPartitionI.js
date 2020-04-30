9f9969d1db5ae718c006de3980e017504215be95
peter <peterwangjiawei@gmail.com>
2020-04-29 22:56:01 -0400

    Apr 29 Leetcode

--- Array/Lc561_ArrayPartitionI.js
+++ Array/Lc561_ArrayPartitionI.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-
+    nums.sort((x, y) => x - y)
+    let res = 0;
+    for(let i = 0; i < nums.length; i += 2){
+        res += nums[i];
+    }
+    return res;
 };
