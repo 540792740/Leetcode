a0bb61e743fe169201fab9d9bbc9466f9be7bf2d
peter <peterwangjiawei@gmail.com>
2020-07-20 11:36:12 -0500

    Jul 20 LeetCode 123456

--- JS_String/Lc005_LongestPalindromicSubstring.js
+++ JS_String/Lc005_LongestPalindromicSubstring.js
@@ -1,4 +1,30 @@
+
+
 var longestPalindrome = function(s) {
+    let res = '';
+    let ls = s.length
+    const helper = (l, r)=>{
+        while(l > -1 && r < ls && s[l] === s[r]){
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+    for(let i = 0; i < ls; i++) {
+        //Even
+        let temp_s = helper(i, i + 1)
+        if(temp_s.length > res.length) res = temp_s;
+
+        //Odd
+        temp_s = helper(i, i)
+        if(temp_s.length > res.length) res = temp_s;
+    }
+    return res
+
+}
+
+
+var longestPalindrome1 = function(s) {
     let ls = s.length;
     if (ls <= 1) return s;
     let maxLen = 0
@@ -14,7 +40,6 @@ var longestPalindrome = function(s) {
             }
             if(right - left + 1 > maxLen){
                 maxLen = right - left + 1
-                console.log(left + 1, right - left)
                 res = s.substring(left + 1, right)
             }
         }
