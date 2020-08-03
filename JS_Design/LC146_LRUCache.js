e8f655146b241e79fa68ca8dc58ec4da63378827
peter <peterwangjiawei@gmail.com>
2020-08-03 14:48:34 -0500

    Aug 3 LeetCode 12

--- JS_Design/LC146_LRUCache.js
+++ JS_Design/LC146_LRUCache.js
@@ -1,18 +1,23 @@
 var LRUCache = function(capacity) {
     this.map = new Map();
-    // this.arr = [];
+    this.capacity = capacity;
 };
 
 LRUCache.prototype.get = function(key) {
-    if(this.map.has(key)){return this.map.get(key)}
+    if(this.map.has(key)){
+        let value = this.map.get(key);
+        this.map.delete(key);
+        this.map.set(key, value);
+        return this.map.get(key)
+    }
     else return -1
 };
 
 LRUCache.prototype.put = function(key, value) {
-    if(this.map.has(key)){}
-    else{
-        let ls = this.arr.length;
-        this.map.set(key, [ls , value])
-        this.arr.push()
+    if(this.map.has(key)){this.map.delete(key)}
+
+    this.map.set(key, value)
+    if(this.capacity < this.map.size){
+        this.map.delete(this.map.keys().next().value);
     }
 };
