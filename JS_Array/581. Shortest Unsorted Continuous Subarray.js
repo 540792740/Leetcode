7099eb74c023dd4d53683f41a1b25eb4e1f7541e
peter <peterwangjiawei@gmail.com>
2020-08-28 17:24:45 -0500

    Aug 28 LeetCode 1234

--- JS_Array/581. Shortest Unsorted Continuous Subarray.js
+++ JS_Array/581. Shortest Unsorted Continuous Subarray.js
@@ -25,5 +25,24 @@ var findUnsortedSubarray = function(nums) {
 
 };
 
-console.log(findUnsortedSubarray( [2, 6, 4, 8, 10, 9, 15]))
-console.log(findUnsortedSubarray([1,2,3,4]))
+// console.log(findUnsortedSubarray( [2, 6, 4, 8, 10, 9, 15]))
+// console.log(findUnsortedSubarray([1,2,3,4]))
+
+
+var findUnsortedSubarray1 = function(nums) {
+    let l = 0, r = nums.length-1, nd = -1, st = 0;
+    let min  = Infinity, max = -Infinity;
+
+    while(r>=0){
+        nums[l] >= max ? max = nums[l] : nd = l;
+        nums[r] <=min ? min = nums[r] : st = r;
+        l++;
+        r--;
+    }
+
+    return nd-st+1;
+
+}
+
+console.log(findUnsortedSubarray1( [2, 6, 4, 8, 10, 9, 15]))
+console.log(findUnsortedSubarray1([1,2,3,4]))
