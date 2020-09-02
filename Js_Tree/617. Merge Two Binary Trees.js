ce5f32fa356bb6e2799bc2052f4c2937d3670203
peter <peterwangjiawei@gmail.com>
2020-09-02 15:22:17 -0500

    Sep 2 LeetCode 12

--- Js_Tree/617. Merge Two Binary Trees.js
+++ Js_Tree/617. Merge Two Binary Trees.js
@@ -10,18 +10,31 @@ function TreeNode(val, left, right) {
      this.right = (right===undefined ? null : right)
 }
 
-var mergeTrees = function(t1, t2) {
-    // if(t1) return t2;
-    // if(t2) return t1;
-    if(t1 && t2) {
-        let root = new TreeNode(t1.val + t2.val);
-        root.left = mergeTrees(t1.left, t2.left);
-        root.right = mergeTrees(t1.right, t2.right);
-        return root;
-    }
-    return t1 || t2
 
+var mergeTrees = function(t1, t2) {
+    if(!t1) return t2;
+    if(!t2) return t1;
+    t1.val += t2.val;
+
+    t1.left = mergeTrees(t1.left, t2.left)
+    t1.right = mergeTrees(t1.right, t2.right)
+
+    return t1
 };
+
+
+// var mergeTrees1 = function(t1, t2) {
+//     // if(t1) return t2;
+//     // if(t2) return t1;
+//     if(t1 && t2) {
+//         let root = new TreeNode(t1.val + t2.val);
+//         root.left = mergeTrees(t1.left, t2.left);
+//         root.right = mergeTrees(t1.right, t2.right);
+//         return root;
+//     }
+//     return t1 || t2
+//
+// };
 
 let a = new TreeNode(1)
 let b = new TreeNode()
