ad8a8b121fa4bd5a7e6fb6a2f9f7c8c07ac128dd
peter <peterwangjiawei@gmail.com>
2020-09-08 20:51:35 -0500

    Sep 8 LeetCode 12

---
+++ JS_LinkedList/234. Palindrome Linked List.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+      this.next = (next===undefined ? null : next)
+}
+
+var isPalindrome1 = function(head) {
+    let res = '';
+    let res_palindrome = ''
+    while(head){
+        res += head.val;
+        res_palindrome = head.val + res_palindrome
+        head = head.next
+    }
+    return res === res_palindrome;
+};
+var isPalindrome = function(head) {
+    if(!head || !head.next) return true
+    let fast = head;
+    let backPointer = new ListNode();
+    let slow = head;
+    let temp
+
+    while(fast && fast.next){
+        fast = fast.next.next;
+
+        // Reverse it
+        temp = slow.next;
+        slow.next = backPointer;
+        backPointer = slow;
+        slow = temp;
+    }
+
+    // Even
+    if(fast !== null){
+        slow = slow.next
+    }
+
+    while(slow && backPointer){
+        if(slow.val !== backPointer.val) return false;
+        slow = slow.next;
+        backPointer = backPointer.next
+    }
+    return true;
+}
