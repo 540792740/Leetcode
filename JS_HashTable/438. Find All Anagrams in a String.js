4ac3d1fbd4d744fa91b2fbb619154a81b23e380d
peter <peterwangjiawei@gmail.com>
2020-09-20 22:46:05 -0500

    Sep 18 LeetCode 2

---
+++ JS_HashTable/438. Find All Anagrams in a String.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ *
+ *  abc => Set {acb, bac, bca, cba, cab}
+ *  check all.
+ *
+ */
+var findAnagrams = function(s, p) {
+    const findAnagrams = (str) => {
+        let set = new Set();
+        const helper = (p, str) => {
+            // for(let )
+        }
+        return
+    }
+
+    let p_anagrams = findAnagrams(p);
+
+};
