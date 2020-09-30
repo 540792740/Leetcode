4899d4248d9c8bd416268c8876a0d341532e4b32
peter <peterwangjiawei@gmail.com>
2020-09-30 11:35:55 -0500

    Sep 30 LeetCode 12

--- JS_HashTable/438. Find All Anagrams in a String.js
+++ JS_HashTable/438. Find All Anagrams in a String.js
@@ -8,21 +8,35 @@
  *
  */
 var findAnagrams = function(s, p) {
-    const findAnagrams = (p_rem, str) => {
-        console.log(str)
-        if(p_rem.length === 0) {
-            set.add(str)
+
+    let dic = {};
+    let res = [];
+    let uniqueChar = 0;
+
+    let ls1 = s.length;
+    let ls2 = p.length;
+    if(ls1 < ls2) return [];
+
+    for(let i of p){
+        if(dic[i] == null) {
+            dic[i] = 1;
+            uniqueChar++;
         }
-        for(let i in p_rem){
-            let strTemp = str + p_rem[i];
-            let temp = p_rem.slice(0, i) + p_rem.slice(i + 1)
-            findAnagrams(temp, strTemp)
+        else dic[i] = dic[i] + 1;
+    }
+
+    let l = 0;
+    let r = 0;
+    for(r; r < ls1; r++){
+        if(dic[s[r]] !== null) dic[s[r]]--;
+        if(dic[s[r]] === 0) uniqueChar--;
+        if(uniqueChar === 0) res.push(l);
+        if(r - l + 1 === ls2){
+            if(dic[s[l]] !== null) dic[s[l]]++;
+            if(dic[s[l++]] === 1)uniqueChar++;
         }
     }
-    let set = new Set();
-    let p_anagrams = findAnagrams(p, '');
-    console.log(set)
-
+    return res;
 };
 
 console.log(findAnagrams("cbaebabacd", "abc"))
