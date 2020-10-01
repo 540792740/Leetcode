91c27ddab310dae7be4e7cc821cd0169ecd54782
peter <peterwangjiawei@gmail.com>
2020-10-01 11:48:55 -0500

    Nov 1 LeetCode 12

--- JSTest/Banhua.js
+++ JSTest/Banhua.js
@@ -17,15 +17,22 @@ function JsonCompare(data1, data2) {
 
     // Initial Stores and field;
     res['stores'] = [];
-    res['field'] = [];
+    res['fields'] = [];
 
-    // save id into set
+    // save id and attributes into set
     let data1_id = new Set()
     let data2_id = new Set()
+    let data1_attributes = new Set()
+    let data2_attributes = new Set()
+
+    // Operation 1: deal with store
 
     // fill set1 of mockDataset
     for(let i of data1.data) {
         data1_id.add(i.id);
+        for(let j in i.attributes){
+            data1_attributes.add(j);
+        }
     }
 
     // fill set2 of prodDataset, find missing from prod
@@ -35,6 +42,10 @@ function JsonCompare(data1, data2) {
         temp['storeId'] = i.id;
         temp['missingFrom'] = 'prod';
         if (!data1_id.has(i.id)) res['stores'].push(temp)
+
+        for(let j in i.attributes){
+            data2_attributes.add(j);
+        }
     }
 
     //find missing from mock
@@ -45,44 +56,26 @@ function JsonCompare(data1, data2) {
         if (!data2_id.has(i.id)) res['stores'].push(temp)
     }
 
-    // console.log(data1_id[0])
-    // console.log(data2_id[0])
-
-    //     let id = data1.data[i].id
-    //     console.log(id)
-    //     for(let j in data2.data){
-    //         if(data2.data[j].id === id){
-    //             let tempRes = {};
-    //             let dataType = data1.data[i].type;
-    //             tempRes[dataType] = [];
-    //
-    //
-    // //         }
-    //         console.log(data1.data[i])
-    //         console.log(data2.data[j])
-    //         break;
-    //     }
-    // break
-
-    //     console.log(data2.data.length)
-        // console.log(data2.data[i]['id'])
-    // }
-
+    // Operation 2: deal with field
+    let intersectData = new Set([...data2_attributes].filter(x => !data1_attributes.has(x)))
+    for(let i of intersectData){
+        res['fields'].push(i);
+    }
     return res;
 
 }
 
 console.log(JsonCompare(mockData, prodData));
+// JsonCompare(mockData, prodData)
 
 
 
-
-fs.writeFile('./mock_copy.json',mockData,'utf8',function(err){
-    if(err)
-        console.log('写文件出错了，错误是：'+err);
-    else
-        console.log('ok');
-})
+// fs.writeFile('./mock_copy.json',mockData,'utf8',function(err){
+//     if(err)
+//         console.log('写文件出错了，错误是：'+err);
+//     else
+//         console.log('ok');
+// })
 
 
 
