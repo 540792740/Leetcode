70e8c134e0471bdb6575835daba582e766223e89
peter <peterwangjiawei@gmail.com>
2020-10-08 17:03:55 -0500

    Oct 9 LeetCode 12334

--- JS_Array/128. Longest Consecutive Sequence.js
+++ JS_Array/128. Longest Consecutive Sequence.js
@@ -3,5 +3,22 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-
+    if(nums.length === 0) return 0
+    let res = 1;
+    nums.sort((x, y) => x - y);
+    let temp = nums[0]
+    let count = 1;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] === nums[i - 1]) continue;
+        if(nums[i] === temp + 1) {
+            count++
+        }
+        else{
+            count = 1;
+        }
+        if(count > res) res = count;
+        temp = nums[i];
+    }
+    return res;
 };
+console.log(longestConsecutive([100, 4, 200, 1, 3, 2]))
