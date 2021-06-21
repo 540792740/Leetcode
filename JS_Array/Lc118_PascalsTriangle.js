8d7e4474f6f5af0e9c7d2610eb572002ec27fcac
540792740 <peterwangjiawei@gmail.com>
2021-06-21 11:10:08 -0400

    name

--- JS_Array/Lc118_PascalsTriangle.js
+++ JS_Array/Lc118_PascalsTriangle.js
@@ -2,11 +2,11 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-var generate = function(numRows) {
-    if (numRows === 0){return []}
-    else if(numRows === 1){return [[1]]}
-    else if(numRows === 2){return [[1], [1, 1]]}
-    else{
+var generate = function (numRows) {
+    if (numRows === 0) { return [] }
+    else if (numRows === 1) { return [[1]] }
+    else if (numRows === 2) { return [[1], [1, 1]] }
+    else {
         let index = 3
         let temp = [1]
         var res = [[1], [1, 1]];
@@ -20,12 +20,4 @@ var generate = function(numRows) {
             res.push(temp)
         }
     }
-    var resString = ''
-    for (let i = 0; i < res.length; i++){
-        for (let j = 0; j < res[i].length; j++){
-            resString += JSON.stringify(res[i][j])
-        }
-    }
-    return resString;
-};
-a = "a,b,b"
+}
