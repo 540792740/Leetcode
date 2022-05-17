5f477a47efbefaa102d0ebb7a27aa00c5261bef0
540792740 <peterwangjiawei@gmail.com>
2022-05-17 08:35:40 +0800

    1, 36

--- JS_Array/1_TwoSum.js
+++ JS_Array/1_TwoSum.js
@@ -3,15 +3,14 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let dic = {};
-    for(let i = 0; i < nums.length; i++){
-        console.log(dic)
-        if(dic[nums[i]] !== undefined) return [dic[nums[i]], i];
-        else{
+    for (let i = 0; i < nums.length; i++) {
+        if (dic[nums[i]] !== undefined) return [dic[nums[i]], i];
+        else {
             dic[target - nums[i]] = i;
         }
     }
 };
-console.log(twoSum([2,8,4,7], 9))
+console.log(twoSum([2, 8, 4, 7], 9))
 
---
+++ JS_Array/36.isValidSudoku.js
@@ -0,0 +1,28 @@
+
+var isValidSudoku = function (board) {
+  let row = [];
+  let column = [];
+  let square = [];
+
+  for (let i = 0; i < 9; i++) {
+    row.push(new Array(9))
+    column.push(new Array(9))
+    square.push(new Array(9))
+  }
+
+  let num // 记录当前数
+  let k // 记录当前9宫格位置
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (board[i][j] !== ".") {
+        num = board[i][j] - 1;
+        k = Math.floor(i / 3) + Math.floor(j / 3) * 3
+        if (row[num][j] || column[i][num] || square[k][num]) return false
+        row[num][j] = column[i][num] = square[k][num] = 1;
+      }
+    }
+  }
+  return true
+}
+
+isValidSudoku([["5", "3", ".", ".", "7", ".", ".", ".", "."], ["6", ".", ".", "1", "9", "5", ".", ".", "."], [".", "9", "8", ".", ".", ".", ".", "6", "."], ["8", ".", ".", ".", "6", ".", ".", ".", "3"], ["4", ".", ".", "8", ".", "3", ".", ".", "1"], ["7", ".", ".", ".", "2", ".", ".", ".", "6"], [".", "6", ".", ".", ".", ".", "2", "8", "."], [".", ".", ".", "4", "1", "9", ".", ".", "5"], [".", ".", ".", ".", "8", ".", ".", "7", "9"]])
