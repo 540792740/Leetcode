94d482be1a492d620b24b1690a1f153a91b496b9
540792740 <peterwangjiawei@163.com>
2023-05-31 18:37:50 +0800

    wework

--- interview/wework.js
+++ interview/wework.js
@@ -1,7 +1,5 @@
 var reversStr = function (str) {
-    let resStr = '';
-    resStr = str.substring(0, Math.ceil(str.length / 2)) + str.substring(Math.ceil(str.length / 2)).split('').reverse().join('');
-    console.log(resStr);
+    return str.substring(0, Math.ceil(str.length / 2)) + str.substring(Math.ceil(str.length / 2)).split('').reverse().join('');
 };
 console.log(reversStr('abcdef'), 'abcfed')
 console.log(reversStr('abcdefg'), 'abcfed')
